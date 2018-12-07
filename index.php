9b90e20693943a225ea2c6861f6e3bf7c1a78390
Kye Buffery <kyebuffery14@gmail.com>
2018-12-07 12:15:16 +0000

    reduced index.php and added App controller

--- index.php
+++ index.php
@@ -2,15 +2,4 @@
 
 require 'vendor/autoload.php';
 
-use App\FrontController;
-
-$session = new FrontController();
-
-$session->set_url("$_SERVER[REQUEST_URI]");
-
-$session->render();
-
-function get_partial($filename) 
-{
-    include "src/view/partials/$filename.php";
-}
+require 'src/App.php';
---
+++ src/App.php
@@ -0,0 +1,14 @@
+<?php 
+
+use App\FrontController;
+
+$session = new FrontController();
+
+$session->set_url("$_SERVER[REQUEST_URI]");
+
+$session->render();
+
+function get_partial($filename) 
+{
+    include "src/view/partials/$filename.php";
+}
